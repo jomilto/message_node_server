a235a4f5e4d081062bc2bcf6e20f48503ee66401
Jose Lobato <joselobatof@gmail.com>
2022-01-24 00:13:26 +0000

    mongodb atlas connection

--- components/message/store.js
+++ components/message/store.js
@@ -1,11 +1,29 @@
-const list = [];
+const db = require("mongoose");
+const Model = require("./model");
+
+db.Promise = global.Promise;
+const options = {
+    keepAlive: 1,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+};
+
+db.connect("mongodb+srv:///messages?retryWrites=true&w=majority", options)
+    .then(() => console.log('DB connected'))
+    .catch((err) => {
+        console.log(err);
+    });
 
 function addMessage(message) {
-    list.push(message);
+    // list.push(message);
+    const myMessage = new Model(message);
+    myMessage.save();
+    // return myMessage;
 }
 
-function getMessages() {
-    return list;
+async function getMessages() {
+    let messages = await Model.find();
+    return messages;
 }
 
 module.exports = {
